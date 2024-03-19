ea2fd3819e5883eb9830a1dd07803296ccf98e77
Randall Reichenberger <randall.reichenberger1@gmail.com>
2024-03-19 17:24:41 -0500

    created base pages and logic for db / model

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User
+# from forms import 
+# from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.app_context().push()
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
